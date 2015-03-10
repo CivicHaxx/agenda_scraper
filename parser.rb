b9d134e9769d92db73a61a74f8d2e8b5561df8e8
CivicImages <kobewka@gmail.com>
2015-03-10 16:09:54 -0400

    Parsing item_number and item_type

--- parser.rb
+++ parser.rb
@@ -3,16 +3,36 @@ require	"awesome_print"
 require "pry"
 
 file = File.open("agendas/9688.html").read
-sections = file.scrub.split("<br clear=\"all\">     <table class=\"border\`"")
+sections = file.scrub.split("<br clear=\"all\">")
 
 preview = sections.map do |section|
-	"<div style='border: 2px solid red; margin-bottom: 50px'> <table class=\"border\"#{section}</div>"
+	"<div style=\"border: 2px solid red; margin-bottom: 50px\">#{section}</div>"
 end.join
 
-File.open("tester.html", 'w') {|f| f.write(preview) }
+File.open("preview.html", 'w') {|f| f.write(preview) }
 
-items = sections.map { |section| Nokogiri::HTML(section) }
+items = sections.map { |item| Nokogiri::HTML(item) }
+
+items.each do |item|
+	item_number = item.xpath('//table[@class="border"]/tr/td/font[@size="5"]').text
+	unless item_number.empty?
+		item_type = item.xpath('//table[@class="border"]/tr/td/p/font').first.text
+	end
+end
+
+item.xpath('//table[@class="border"]/tr/td > td/font[@size="5"]').text
+
 
 binding.pry
 
 print ""
+
+
+
+# - councillor_id (member motion)
+# - summary
+# - recommendation
+# - decision_text
+# - background information 
+# - ward
+
