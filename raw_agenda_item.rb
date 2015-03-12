6a2401b0afc76e399a7ccb6ebe333b43243c4969
CivicImages <kobewka@gmail.com>
2015-03-12 17:06:39 -0400

    refactoring RawAgenda Item

--- raw_agenda_item.rb
+++ raw_agenda_item.rb
@@ -1,19 +1,17 @@
 class RawAgendaItem
 	def self.parse(item_number, item)
-		item_type   = item.xpath("//table[@class='border']/tr/td/p/font").first.text.capitalize.chop
-		ward        = item.xpath("//table[@class='border']/tr/td/p/font").last.text.chop
-		item_title  = item.xpath('//table/tr/td/font/b').first.text
-		item_tables = item.xpath('//table')
-		raw_html    = item.xpath('//table')[2..item_tables.length]	
-		new(number: item_number, type: item_type, ward: ward, title: item_title, contents: raw_html)
+		item_type           = item.xpath("//table[@class='border']/tr/td/p/font").first.text.capitalize.chop
+		ward                = item.xpath("//table[@class='border']/tr/td/p/font").last.text.chop
+		item_title          = item.xpath('//table/tr/td/font/b').first.text
+		item_tables         = item.xpath('//table')
+		raw_html            = item.xpath('//table')[2..item_tables.length]
+		sections            = hash_sections(raw_html)
+		recommendations     = sections[:recommendations]
+
+		new(number: item_number, type: item_type, ward: ward, title: item_title, recommendations: recommendations, sections: sections)
 	end
 
-	def initialize(number: nil, type: nil, ward: nil, title: nil, contents: nil)
-		@number 	= number
-		@type 		= type
-		@ward 		= ward
-		@title 		= title
-		
+	def self.hash_sections(contents)
 		keywords = [
 			"Recommendations",
 			"Decision Advice and Other Information",
@@ -28,18 +26,29 @@ class RawAgendaItem
 		sections = Hash.new('')
 		current_section = ""
 
-		@contents = contents.css('td').map do |node|
+		contents.css('td').map do |node|
 			if node.css('p').length > 0
 				sections[current_section] << node.css('p').map(&:text).join(" ")
 			elsif node.css('b').length > 0 && keywords.any? { |keyword| node.text[keyword] }
-				current_section = node.text
+				current_section = node.text.downcase.gsub(" ", "_").to_sym
 				sections[current_section] = ""
 			else
 				sections[current_section] << node.to_s
 			end
 		end.flatten
+		sections
 	end
 
+	def initialize(number: nil, type: nil, ward: nil, title: nil, recommendations: nil, sections: nil )
+		@number          = number
+		@type            = type
+		@ward            = ward
+		@title           = title
+		@recommendations = recommendations
+		@sections        = sections
+		binding.pry
+	end
+	
 	def to_s
 		[
 			"Number: #{@number}",
