5efb64745feb1131fc8bd3f222ee8ceda657cce9
CivicImages <kobewka@gmail.com>
2015-03-12 11:35:03 -0400

    Modifying calendar page request to get a page with all the meetings from a year

--- seed_file.rb
+++ seed_file.rb
@@ -22,20 +22,15 @@ def calendar_params(month, year)
 end
 
 calendar_uri = URI("#{BASE_URI}meetingCalendarView.do")
-meeting_ids  = []
 
-# 12.times do |i|
-  calendar_page = Net::HTTP.post_form(calendar_uri, calendar_params(12, 2014)).body
-  page          = Nokogiri::HTML(calendar_page)
-  anchors       = page.css("#calendarList .list-item a")
-  anchors       = anchors.to_ary
-  
-  meeting_ids << anchors.map do |a|
-    a.attr("href").split("=").last if a.text.include? "City Council"
-  end.reject(&:nil?).uniq
-# end
+calendar_page = Net::HTTP.post_form(calendar_uri, calendar_params(12, 2014)).body
+page          = Nokogiri::HTML(calendar_page)
+anchors       = page.css("#calendarList .list-item a")
+anchors       = anchors.to_ary
 
-meeting_ids.flatten!
+meeting_ids = anchors.map do |a|
+  a.attr("href").split("=").last if a.text.include? "City Council"
+end.reject(&:nil?).uniq.flatten
 
 puts "I found #{meeting_ids.length} meeting IDs."
 binding.pry
